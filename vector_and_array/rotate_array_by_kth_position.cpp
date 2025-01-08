7b5b63aa0c5ccd95f44d0c9878a4d1f869c2fe4d
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-01-08 23:45:04 +0530

    kth_rotation_in_an_array

--- vector_and_array/rotate_array_by_kth_position.cpp
+++ vector_and_array/rotate_array_by_kth_position.cpp
@@ -1,30 +1,50 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-//1 2 3 4 5 6 7
-//k=2
-//3 4 5 6 7 1 2
-void kth_rotation(vector<int>&v,int k,vector<int>&new_array){
-    int sizee=v.size()-1;
-    for(auto i=0;i<v.size();i++){
-        v[i+1+k]=new_array[i];
+
+// Function to perform a single rotation
+void kth_rotation(vector<int>& v, vector<int>& new_array) {
+    // Perform one right rotation
+    int temp = v[0];
+    for (int i = 0; i < v.size() - 1; i++) {
+        new_array[i] = v[i + 1];
     }
-    for(auto i=0;i<v.size();i++){
-        cout<<new_array[i]<<" ";
+    new_array[v.size() - 1] = temp;
+
+    // Copy new_array back to v
+    for (int i = 0; i < v.size(); i++) {
+        v[i] = new_array[i];
     }
 }
-int main(){
-    vector<int>v;
-    vector<int>new_array;
-    int n,x,k;
-    cout<<"Enter the number of elements in the element in array: ";
-    cin>>n;
-    for(auto i=0;i<n;i++){
-        cin>>x;
-        v.push_back(x); 
+
+int main() {
+    vector<int> v;
+    int n, x, k;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    v.resize(n); // Resize the vector to hold n elements
+    vector<int> new_array(n); // Resize new_array to hold n elements
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
     }
-    cout<<"Enter the kth rotation element ";
-    cin>>k;
-    kth_rotation(v,k,new_array);
+
+    cout << "Enter the kth rotation element: ";
+    cin >> k;
+
+    // Perform k rotations
+    for (int i = 0; i < k; i++) {
+        kth_rotation(v, new_array);
+    }
+
+    // Print the rotated array
+    for (int i = 0; i < n; ++i) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
