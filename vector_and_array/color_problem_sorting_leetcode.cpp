b8c2bc8d5727e6e0c3d1aa53348057b1b250df3a
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-01-09 13:05:28 +0530

    leetcode_problem_85

---
+++ vector_and_array/color_problem_sorting_leetcode.cpp
@@ -0,0 +1,21 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//        int start=0,mid=0,end=nums.size()-1;
+//        while(mid<=end){
+//         if(nums[mid]==0){
+//             swap(nums[start],nums[mid]);
+//             mid++;
+//             start++;
+//         }
+//         else if(nums[mid]==1){
+//             mid++;
+//         }
+//         else if(nums[mid]==2){
+//             swap(nums[mid],nums[end]);
+//             end--;
+            
+//         }
+//        }
+//     }
+// };
