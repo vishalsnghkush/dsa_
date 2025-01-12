60341668c234639aec5d5706bce9a411247ee88d
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-01-12 15:18:18 +0530

    spiral_matrix

---
+++ 2D_array/spiral_matrix.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void spiral(int matrix[4][4]) {
+    int top = 0, left = 0, right = 3, bottom = 3;
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            cout << matrix[top][i] << " ";
+        }
+        top++;
+        for (int i = top; i <= bottom; i++) {
+            cout << matrix[i][right] << " ";
+        }
+        right--;
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                cout << matrix[bottom][i] << " ";
+            }
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                cout << matrix[i][left] << " ";
+            }
+            left++;
+        }
+    }
+}
+
+int main() {
+    int matrix[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+    spiral(matrix);
+    return 0;
+}
