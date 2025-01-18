ca075d5aa5a3e45456d496c6cecdf1f0766e1157
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-01-18 19:02:10 +0530

    sqrt

---
+++ binary_search/sqrt.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int sqrt(int n){
+    int start=0;
+    int end=n;
+    int ans=-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(mid*mid==n) return ans=mid;
+        else if(mid*mid<n){
+            start=mid+1;
+            ans=mid;
+        }
+        else 
+        end=mid-1;
+    }
+    return ans;
+}
+int main(){
+    // sqrt ...
+    int n=1010;
+    cout<<sqrt(n);
+}
