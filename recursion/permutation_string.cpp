029fc50a5ff282287b1f195324804c4a2d026ee3
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-01-18 12:15:47 +0530

    strig permutauion

---
+++ recursion/permutation_string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void permutation(string ans,string original){
+    if(original==""){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0;i<original.length();i++){
+        char ch=original[0];
+        string left=original.substr(0,i);
+        string right=original.substr(0,i+1);
+        permutation(ans+ch,left+right);
+    }
+}
+int main(){
+    string str="abcd";
+    permutation("",str);
+    return 0;
+}
