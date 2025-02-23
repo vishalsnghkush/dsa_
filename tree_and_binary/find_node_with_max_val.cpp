992960a0c2446c7be2ed7b9739c6b20ae2666c8a
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-02-23 12:56:22 +0530

    max_val

---
+++ tree_and_binary/find_node_with_max_val.cpp
@@ -0,0 +1,56 @@
+//only root tree ka address given hoga
+
+#include<iostream>
+#include<climits>
+using namespace std;
+class Node{ //this is tree node
+public:
+    int val;
+    Node *left;
+    Node *right;
+Node(int val){
+    this->val=val;
+    this->left=NULL;
+    this->right=NULL;
+}   
+};
+int sum(Node *root){
+    if(root==NULL) return 0;
+    int leftsum=sum(root->left);
+    int rightsum=sum(root->right);
+    int ans=root->val+leftsum+rightsum;
+    return ans;    
+}
+int size(Node *root){
+    // no of nodes
+    if(root==NULL) return 0;
+    return 1+size(root->right)+size(root->left);
+
+}
+void displayTree(Node *root){
+    if(root==NULL) return;
+    cout<<root->val<<" ";
+    displayTree(root->left);
+    displayTree(root->right);
+}
+int maxVal(Node *root){
+    if(root==NULL) return INT_MIN;
+    return max((root->val),max(maxVal(root->left),maxVal(root->right)));
+}
+int main(){
+    Node *a=new Node(1);
+    Node *b=new Node(2);
+    Node *c=new Node(3);
+    Node *d=new Node(4);
+    Node *e=new Node(5);
+    Node *f=new Node(6);
+    Node *g=new Node(7);
+    a->left=b;
+    a->right=c;
+    b->left=d;
+    b->right=e;
+    c->left=f;
+    c->right=g;
+    cout<<maxVal(a);
+    return 0;
+}
