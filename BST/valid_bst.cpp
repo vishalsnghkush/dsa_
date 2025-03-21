8f3e433eb5cf4ebe28979e5878dc7475f7250ede
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-03-21 10:28:18 +0530

    valif

---
+++ BST/valid_bst.cpp
@@ -0,0 +1,7 @@
+// first way is to do by comparing the value of left and right if the value of left is less than the right max(of) then it is true 
+// and simplarly the min of right side is greater than that of root
+
+// second way is to fill the vector of inorder
+// since the inorder tranverse give the sorted form 
+// if the vector is in sorted form then it is sorted form 
+// that's it
