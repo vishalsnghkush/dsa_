6814ba967b8c2ae35762d2f3b46d5c64f52f6779
Vishal Kushwaha <vishalsnghkush31@gmail.com>
2025-03-24 16:55:04 +0530

    introduction

--- set/introduction_.cpp
+++ set/introduction_.cpp
@@ -1,13 +1,20 @@
 /*data structure with unique property
 
+it store the unique element only 
+
 
 insert        O(1)
 search        O(1)
 erase/delete  O(1)
 
 not an linear data structure
+it stores element at random order
 
 s.insert(1)
+
+
+for triversing the element of the sets 
+we use for each loop
 
 */
 
@@ -16,5 +23,30 @@ s.insert(1)
 using namespace std;
 int main(){
     unordered_set<int>s;
-    
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(5);
+    // size
+    cout<<s.size()<<endl; // print 5 becaz it ignore the repeated value
+
+
+    s.erase(2); // 2 ko erase kr diya gya
+
+    for(int ele : s){ // read as ele in s (sets)  .... data type likhte hai i.e int tha
+        cout<<ele<<" ";
+    }
+
+    // for finding the target
+    // syntax for the finding the element to the sets 
+    // s.find() -> it search in the set and if
+    // it is not found then it returns the last element 
+    int target=3;
+    if(s.find(target)!=s.end()){ // target exists   
+        cout<<"Exists !";
+    }
+    else cout<<"Does not Exists !";
+
 }
